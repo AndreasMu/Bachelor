4534ad1070f91a327c1c560ff1a2ede0e15d9946
Dakra23 <s6asmuel@astro.uni-bonn.de>
2014-11-28 17:41:18 +0100

    ende

--- Lumiradius.py
+++ Lumiradius.py
@@ -337,15 +337,15 @@ def Lumiradius(M,Z,Age):
     logR=aR*Age+bR*Age**10+c*Age**40+(math.log10(RTMS/RZAMS)-aR-bR-c)*Age**3-dR*(t1**3-t2**3)+math.log10(RZAMS)
 
     #Here to test the function
-    arrayR[Iteration]=logR
-    arrayL[Iteration]=logL
-    arrayT[Iteration]=Age
-    print"LTMS=%g"%LTMS
+    #arrayR[Iteration]=logR
+    #arrayL[Iteration]=logL
+    #arrayT[Iteration]=Age
+    #print"LTMS=%g"%LTMS
     #End of calculations!
     return (logL, logR)
 
 #Testing whether this function actually works
-arrayR=np.array(range(101))
+"""arrayR=np.array(range(101))
 arrayR=arrayR.astype(float)
 arrayL=np.array(range(101))
 arrayL=arrayL.astype(float)
@@ -362,4 +362,4 @@ while(Iteration<101):
 plt.plot(arrayT, arrayR)
 plt.xlabel('t/tms')
 plt.ylabel('log(R)')
-plt.show()
+plt.show()"""
