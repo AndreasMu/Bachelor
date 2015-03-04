6176816849db354eddec0a6d0b2bd8ce37a899f5
Dakra23 <s6asmuel@astro.uni-bonn.de>
2015-03-04 18:50:35 +0100

    testfunctionsandstuff

--- Programs/Grid.py
+++ Programs/Grid.py
@@ -25,7 +25,7 @@ rangeage = 100 #Amount of intervals I have for age
 #This function is an excerpt from Lumiradius.py. Since Z=0.02 in our case the formulas
 #get rather simple in comparison.
 #tms is in units of Myr
-def Mainsequenceage(mass):
+def Mainsequenceage(realmass):
     a1 = 1593.89
     a2 = 2706.708
     a3 = 146.6143
@@ -37,13 +37,14 @@ def Mainsequenceage(mass):
     a9 = 1.312179
     a10 = 0.8073972
     k = max(0.95,min(0.95-0.03*0.30103,0.99))
-    mu = max(0.5,1.0-0.01*max(a6/mass**a7,a8+a9/mass**a10))
-    tbgb = (a1+a2*mass**4+a3*mass**5.5+mass**7)/(a4*mass**2+a5*mass**7)
+    mu = max(0.5,1.0-0.01*max(a6/realmass**a7,a8+a9/realmass**a10))
+    tbgb = (a1+a2*realmass**4+a3*realmass**5.5+realmass**7)/(a4*realmass**2+a5*realmass**7)
     thook = mu*tbgb
     tms = max(thook,k*tbgb)
     return tms
 
 maxage = Mainsequenceage(minmass)
+dlogt=(math.log10(maxage)+1)/rangeage
 
 def Volume(distance):
     distance+=1.
@@ -60,19 +61,17 @@ for distance in range(0,rangedistance):
     volumelabel[distance]=Volume(distance)
     #The volumelabel will save the fractional volume of each shell."""
 
-def IMF(mass):
+def IMF(realmass):
     e= 1.35 * (1/(minmass**-1.35-maxmass**-1.35))
-    massfactor =  math.log(10)*e*mass**(-1.35)
+    massfactor =  math.log(10)*e*realmass**(-1.35)
     return (massfactor)
 
-def Agerelation(mass,realage):
-    #This functions purpose is to make sure I don't include stars, that are already dead.
-    #If the age of the star is greater, than its mainsequence age, the function will return 0.
-    tms = Mainsequenceage(mass)
+def Agerelation(realmass,realage):
+    tms = Mainsequenceage(realmass)
     if(realage>tms):
         agefactor = 0
     else:
-        agefactor = 1
+        agefactor = math.log(10)*realage/tms
     return agefactor
 
 #Here an agerelation matrix is filled so I can easily check, whether a star of
@@ -81,11 +80,10 @@ agerelation = np.zeros(rangemass*rangeage)
 agerelation = agerelation.astype(int)
 agerelation = agerelation.reshape((rangemass,rangeage))
 for mass in range(0,rangemass):
-    masss = 10**(math.log10(minmass) + mass*dlogM)
+    realmass = 10**(math.log10(minmass) + mass*dlogM)
     for age in range(0,rangeage):
-        realage = float(age)*maxage/(rangeage)
-        agerelation[mass,age]=Agerelation(masss,realage)
-
+        realage = 10**(-1.+age*dlogt)
+        agerelation[mass,age]=Agerelation(realmass,realage)
 
 #Here I make two matrices, which will contain Luminosity and Radius of stars of
 #a specific age and mass. In the later parts of this program I can then simply
@@ -96,11 +94,12 @@ logR = logK.reshape((rangemass,rangeage))
 logL = logL.astype(float)
 logR = logR.astype(float)
 for mass in range(0,rangemass):
-    masss = 10**(math.log10(minmass) + mass*dlogM)
+    realmass = 10**(math.log10(minmass) + mass*dlogM)
     for age in range(0,rangeage):
-        fracage = float(age)*maxage/(rangeage*Mainsequenceage(masss))
-        if (agerelation[mass,age]==1):
-            logL[mass,age], logR[mass,age]= LR.Lumiradius(masss, Z, fracage)
+        realage = 10**(-1.+age*dlogt)
+        fracage = realage/Mainsequenceage(realmass)
+        if (agerelation[mass,age]!=0):
+            logL[mass,age], logR[mass,age]= LR.Lumiradius(realmass, Z, fracage)
         else:
             logL[mass,age] = 0
             logR[mass,age] = 0
@@ -154,17 +153,19 @@ masslabel = np.array(range(rangemass))
 masslabel = masslabel.astype(float)    
 for distance in range(0,rangedistance):
     realdistance=distance*maxdistance/rangedistance
-    dV=Volume(distance)*rho*(4./3)*math.pi*dr**3
+    dV=Volume(distance)*(4./3)*math.pi*dr**3
     for mass in range(0,rangemass):
-        masss = 10**(math.log10(minmass) + mass*dlogM)
-        masslabel[mass] = masss
-        mainsequenceage=Mainsequenceage(masss)
-        dM=IMF(masss)*dlogM
+        realmass = 10**(math.log10(minmass) + mass*dlogM)
+        masslabel[mass] = realmass
+        mainsequenceage=Mainsequenceage(realmass)
+        dM=IMF(realmass)*dlogM
         for age in range(0,rangeage):
-            fracage = float(age)*maxage/(rangeage*mainsequenceage)
+            realage = 10**(-1.+age*dlogt)
+            fracage = realage/mainsequenceage
+            dt=Agerelation(realmass,realage)*dlogt
             if Magnitude(logL[mass,age],logR[mass,age],realdistance)<9 and agerelation[mass,age]==1:
                 graphage=int(20*fracage)
-                graph[graphage]+=dV*agerelation[mass,age]*dM
+                graph[graphage]+=dV*dt*dM
 
 agelabel = np.array(range(20))
 agelabel = agelabel.astype(float)
--- Programs/Testing functions/testing functions.py
+++ Programs/Testing functions/testing functions.py
@@ -1,3 +1,15 @@
+
+#Here an agerelation matrix is filled so I can easily check, whether a star of
+#mass x age y actually exists. 
+agerelation = np.zeros(rangemass*rangeage)
+agerelation = agerelation.astype(int)
+agerelation = agerelation.reshape((rangemass,rangeage))
+for mass in range(0,rangemass):
+    masss = 10**(math.log10(minmass) + mass*dlogM)
+    for age in range(0,rangeage):
+        realage = float(age)*maxage/(rangeage)
+        agerelation[mass,age]=Agerelation(masss,realage)
+
 #This will tell me how many entries there are in the agerelation matrix so
 #essentially how many stars I can simulate of a given mass.
 agerange = np.zeros(rangemass)
