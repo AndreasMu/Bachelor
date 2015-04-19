80882e78c106800822016ff9afee7e104bbdf3b2
Dakra23 <s6asmuel@astro.uni-bonn.de>
2015-04-19 18:47:23 +0200

    arf

--- Graphs and Tex/clusterplot.py
+++ Graphs and Tex/clusterplot.py
@@ -7,11 +7,17 @@ clusters=np.loadtxt('clusters.txt')
 logage=clusters[:,1]
 distance=clusters[:,0]
 
+ar=np.arange(0,20.1,0.1)
+al=np.arange(0,20.1,0.1)
+al[:]=math.log10(104e6)
+
+plt.plot(ar,al,'k',linewidth=2)
+
 plt.plot(distance, logage, 'r^')
 #plt.plot(agelabel, compdatanocut, 'k--', label='No magnitude cut')
 #plt.errorbar(agelabel, obsdata, yerr=error,fmt='', label='Observational data')
 #legend=plt.legend(loc='upper left', shadow=True)
 #plt.ylim(ymin=0.1)
 plt.xlabel('distance modulus [mag]')
-plt.ylabel('log(Age/yr)')
+plt.ylabel('log($t$/yr)')
 plt.show()
--- Graphs and Tex/obsdataplot.py
+++ Graphs and Tex/obsdataplot.py
@@ -11,9 +11,9 @@ obsdata=np.loadtxt('obsdata.txt')
 error=obsdata[:,3]
 obsdata=obsdata[:,1]
 
-plt.plot(agelabel, compdata, 'r',label='Computed data')
+#plt.plot(agelabel, compdata, 'r',label='Computed data')
 plt.errorbar(agelabel, obsdata, yerr=error,fmt='',label='Observational data')
-legend = plt.legend(loc='upper left', shadow=True)
+#legend = plt.legend(loc='upper left', shadow=True)
 plt.ylim(ymin=0.1)
 plt.xlabel('$\mathrm{t}/\mathrm{t}_{ms}$')
 plt.ylabel('Probability Density')
